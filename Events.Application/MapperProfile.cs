d5a105f30f2a533a682843413b7bb259836ddfad
Opa Maj <saintkilla79@gmail.com>
2025-04-30 14:14:41 +0300

    Mapper

---
+++ Events.Application/MapperProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Events.Domain.Entities;
+using Events.Domain.Models;
+
+
+namespace Events.Application
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<EventEntity, Event>()
+                .ConstructUsing((src, context) => new Event(
+                    src.Id,
+                    src.Title,
+                    src.Description,
+                    src.HoldedAt,
+                    src.Venue,
+                    src.Category,
+                    src.MaxCountOfParticipant,
+                    context.Mapper.Map<List<Participant>>(src.Participants)
+                    ));
+
+            CreateMap<ParticipantEntity, Participant>();
+
+        }
+    }
+}
