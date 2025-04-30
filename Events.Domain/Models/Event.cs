2d15b282463f1377fc9261e2cbe72e0f466d566c
Opa Maj <saintkilla79@gmail.com>
2025-04-30 10:34:19 +0300

    Добавьте файлы проекта.

---
+++ Events.Domain/Models/Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.Domain.Models
+{
+    public class Event
+    {
+        public Guid Id { get; }
+
+        public string Title { get; }
+
+        public string Description { get; }
+
+        public DateTime HoldedAt { get; }
+
+        public string Venue { get; }
+
+        public Category Category { get; }
+
+        public int MaxCountOfParticipant { get; }
+
+        public List<Participant> Participants { get; }
+    }
+}
---
+++ Events.Domain/Models/Participant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Events.Domain.Models
+{
+    public class Participant
+    {
+        public Guid Id { get; }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        public DateTime DateOfBirth { get; }
+
+        public DateTime DateOfRegistry { get; }
+
+        public string Email { get; }
+    }
+}
