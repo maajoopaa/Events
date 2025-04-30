9a22f615e06d32e10f1a6c5667cff1cef0dfc967
Opa Maj <saintkilla79@gmail.com>
2025-04-30 10:42:24 +0300

    Entities

---
+++ Events.Domain/Entities/EventEntity.cs
@@ -0,0 +1,28 @@
+﻿using Events.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.Domain.Entities
+{
+    public class EventEntity
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public DateTime HoldedAt { get; set; }
+
+        public string Venue { get; set; } = null!;
+
+        public Category Category { get; set; }
+
+        public int MaxCountOfParticipant { get; set; } = 0;
+
+        public List<Participant> Participants { get; set; } = [];
+    }
+}
---
+++ Events.Domain/Entities/ParticipantEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.Domain.Entities
+{
+    public class ParticipantEntity
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public DateTime DateOfBirth { get; set; } 
+
+        public DateTime DateOfRegistry { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public Guid? EventId { get; set; }
+
+        public EventEntity? Event { get; set; }
+    }
+}
