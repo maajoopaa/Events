375e1ad61ac7c4f2917f45ae4544898df1da7089
Opa Maj <saintkilla79@gmail.com>
2025-04-30 15:52:33 +0300

    ServicesExtensions

---
+++ Events.API/ServiceCollectionExtensions.cs
@@ -0,0 +1,71 @@
+﻿using Events.Application;
+using Events.Application.Models;
+using Events.Application.Services;
+using Events.Application.Services.AccountService;
+using Events.Application.Validators;
+using Events.DataAccess;
+using Events.DataAccess.Repositories;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace Events.API
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddDb(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<EventsDbContext>(options => options.UseNpgsql(connectionString));
+        }
+
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddTransient<IEventsRepository, EventsRepository>();
+            services.AddTransient<IParticipantsRepository, ParticipantsRepository>();
+        }
+
+        public static void AddAutoMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MapperProfile));
+        }
+
+        public static void AddValidators(this IServiceCollection services)
+        {
+            services.AddTransient<IValidator<EventRequest>, EventRequestValidator>();
+            services.AddTransient<IValidator<ParticipantLoginRequest>, ParticipantLoginRequestValidator>();
+            services.AddTransient<IValidator<ParticipantRegisterRequest>, ParticipantRegisterRequestValidator>();
+        }
+
+        public static void AddAuth(this IServiceCollection services)
+        {
+            services.AddAuthorization();
+            services.AddAuthentication()
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                        ValidateIssuerSigningKey = true,
+                    };
+                });
+        }
+
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddTransient<IEventService, EventService>();
+            services.AddTransient<IParticipantService, ParticipantService>();
+            services.AddTransient<IAccountService,AccountService>();
+        }
+
+        public static void AddJsonConverting(this IServiceCollection services)
+        {
+            services.AddControllers().AddJsonOptions(options =>
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+        }
+    }
+}
