29ecf67499dbaa3492783a2422774b164acd27f0
Opa Maj <saintkilla79@gmail.com>
2025-04-30 15:52:44 +0300

    Models

---
+++ Events.Application/Models/AccountResponse.cs
@@ -0,0 +1,16 @@
+﻿using Events.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.Application.Models
+{
+    public class AccountResponse
+    {
+        public string? Token { get; set; }
+
+        public Participant? Account { get; set; }
+    }
+}
--- Events.Application/Models/EventRequest.cs
+++ Events.Application/Models/EventRequest.cs
@@ -9,10 +9,14 @@ namespace Events.Application.Models
 
         public string Description { get; set; } = null!;
 
+        public DateTime HoldedAt { get; set; }
+
         public string Venue { get; set; } = null!;
 
         public Category Category { get; set; }
 
         public int MaxCountOfParticipant { get; set; } = 0;
+
+        public string? Image { get; set; }
     }
 }
