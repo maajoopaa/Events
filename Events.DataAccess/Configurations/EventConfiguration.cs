0047087ffad9b619ab77b04665a58e34eb50ecd3
Opa Maj <saintkilla79@gmail.com>
2025-04-30 11:00:15 +0300

    Configurations

---
+++ Events.DataAccess/Configurations/EventConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Events.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.DataAccess.Configurations
+{
+    public class EventConfiguration : IEntityTypeConfiguration<EventEntity>
+    {
+        public void Configure(EntityTypeBuilder<EventEntity> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.HasMany(x => x.Participants)
+                .WithOne(x => x.Event)
+                .HasForeignKey(x => x.EventId);
+        }
+    }
+}
---
+++ Events.DataAccess/Configurations/ParticipantConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Events.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.DataAccess.Configurations
+{
+    public class ParticipantConfiguration : IEntityTypeConfiguration<ParticipantEntity>
+    {
+        public void Configure(EntityTypeBuilder<ParticipantEntity> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.HasIndex(x => x.Email)
+                .IsUnique();
+        }
+    }
+}
