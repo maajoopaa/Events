41828e56d66b2e0f82b0755cc726557f48119482
Opa Maj <saintkilla79@gmail.com>
2025-04-30 15:52:25 +0300

    AutoMapper

--- Events.Application/MapperProfile.cs
+++ Events.Application/MapperProfile.cs
@@ -18,7 +18,8 @@ namespace Events.Application
                     src.Venue,
                     src.Category,
                     src.MaxCountOfParticipant,
-                    context.Mapper.Map<List<Participant>>(src.Participants)
+                    context.Mapper.Map<List<Participant>>(src.Participants),
+                    src.Image
                     ));
 
             CreateMap<ParticipantEntity, Participant>();
