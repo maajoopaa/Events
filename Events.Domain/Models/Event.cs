a67197b2fbecb70b9f34c1360431d1749a7f98dd
Opa Maj <saintkilla79@gmail.com>
2025-04-30 14:15:03 +0300

    Models

--- Events.Domain/Models/Event.cs
+++ Events.Domain/Models/Event.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Events.Domain.Models
+﻿namespace Events.Domain.Models
 {
     public class Event
     {
--- Events.Domain/Models/Participant.cs
+++ Events.Domain/Models/Participant.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
-
-namespace Events.Domain.Models
+﻿namespace Events.Domain.Models
 {
     public class Participant
     {
