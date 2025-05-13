c174217d1962c11c159ce4379e12085b58196073
Opa Maj <saintkilla79@gmail.com>
2025-05-13 14:55:12 +0300

    password column name migration

---
+++ Events.DataAccess/Migrations/20250513114340_PasswordNameChanged.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Events.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class PasswordNameChanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Participants",
+                newName: "PasswordHash");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Participants",
+                newName: "Password");
+        }
+    }
+}
