a2350bb03cb835440416d32e200681393efc3ce7
Opa Maj <saintkilla79@gmail.com>
2025-05-13 14:56:19 +0300

    custom exception for client exceptions

---
+++ Events.Application/Exceptions/ClientException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.Application.Exceptions
+{
+    public class ClientException : Exception
+    {
+
+        public ClientException(string message, int statusCode)
+            : base(message)
+        {
+            Message = message;
+            StatusCode = statusCode;
+        }
+        public string Message { get; set; }
+
+        public int StatusCode { get; set; }
+    }
+}
